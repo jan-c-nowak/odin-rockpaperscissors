a7d3319f8c70fa071c122f39ba96b998f6ed1059
jan-c-nowak <84571576+jan-c-nowak@users.noreply.github.com>
2024-03-21 11:56:59 +0100

    add playRound

--- game.js
+++ game.js
@@ -11,3 +11,20 @@ function getRandomInt(max) {
 function getComputerChoice(){
     return choices[getRandomInt(3)];
 }
+
+// String -> String
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === "rock" && computerSelection === "scissors") {return `You win! ${playerSelection} beats ${computerSelection}`};
+    if(playerSelection === "rock" && computerSelection === "paper") {return `You lose! ${computerSelection} beats ${playerSelection}`};
+
+    if(playerSelection === "paper" && computerSelection === "rock") {return `You win! ${playerSelection} beats ${computerSelection}`};
+    if(playerSelection === "paper" && computerSelection === "scissors") {return `You lose! ${computerSelection} beats ${playerSelection}`};
+
+    if(playerSelection === "scissors" && computerSelection === "paper") {return `You win! ${playerSelection} beats ${computerSelection}`};
+    if(playerSelection === "scissors" && computerSelection === "rock") {return `You lose! ${computerSelection} beats ${playerSelection}`}; 
+
+    if(playerSelection === computerSelection) {return "It's a draw!"};
+}
